4d60a7d787b026156b418edc61204adaef671191
Mauricio Mutte <litiobr@gmail.com>
2019-07-25 20:00:24 -0300

    Fix index.js

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 import Koa from 'koa';
+import Router from 'koa-router';
 import BodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 import routes from './routes/book';
 
 const app = new Koa();
+const router = Router();
 
-app.use(BodyParser({
-  enableTypes: ['json'],
-  jsonLimit: '5mb',
-  strict: true,
-  onerror: function (err, ctx) {
-    ctx.throw('body parse error', 422)
-  }
-}));
+app.use(BodyParser());
 
 // API Route
-app.use(routes);
+app.use(routes).use(router.allowedMethods());
+
 
 // DB Connect
+mongoose.set('useFindAndModify', false);
 mongoose.connect(
   'mongodb://localhost/koa-crud',
   {  useNewUrlParser: true }
