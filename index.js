d19a784f512676f5e0eba4d151772853eb672183
Mauricio Mutte <litiobr@gmail.com>
2019-07-25 20:53:34 -0300

    Add errorHandling middleware

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import BodyParser from 'koa-bodyparser';
 import mongoose from 'mongoose';
 
 import routes from './routes/book';
+import errorHandling from './middleware/errorHandling';
 
 const app = new Koa();
 const router = Router();
@@ -13,6 +14,7 @@ const router = Router();
 app.use(BodyParser());
 app.use(Logger());
 app.use(Cors());
+app.use(errorHandling);
 
 // API Route
 app.use(routes).use(router.allowedMethods());
