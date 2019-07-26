3d729dfbca23908e38c4d710fd3ac0875503c700
Mauricio Mutte <litiobr@gmail.com>
2019-07-25 22:21:25 -0300

    Remove try/catch

--- controllers/book.js
+++ controllers/book.js
@@ -1,47 +1,25 @@
 import Book from '../models/Book';
 
-const error = (err) => {
-  ctx.status = err.status || 500;
-  ctx.body = err.message;
-}
-
 export const find = async ctx => {
-  try {
-    const book = await Book.find({});
-    ctx.body = book;
-  } catch(err) {
-    return error(err);
-  }
+  const book = await Book.find({});
+  ctx.body = book;
 };
 
 export const create = async ctx => {
-  try {
-    const newBook = new Book(ctx.request.body);
-    await newBook.save();
-    ctx.body = newBook;
-  } catch(err) {
-    return error(err);
-  }
+  const newBook = new Book(ctx.request.body);
+  await newBook.save();
+  ctx.body = newBook;
 };
 
 export const remove = async ctx => {
-  try {
-    await Book.deleteOne({_id: ctx.params.id});
-    ctx.body = {message: 'Book was deleted'};
-  } catch(err) {
-    return error(err);
-  }
+  await Book.deleteOne({_id: ctx.params.id});
+  ctx.body = {message: 'Book was deleted'};
 };
 
 export const update = async ctx => {
-  try {
-    const book = await Book.findOneAndUpdate(
-      {_id: ctx.params.id}, 
-      ctx.request.body, 
-      {new: true}
-    );
-    ctx.body = book;
-  } catch (err) {
-    return error(err);
-  }
+  const book = await Book.findOneAndUpdate(
+    {_id: ctx.params.id}, 
+    ctx.request.body, 
+    {new: true}
+  );
 };
